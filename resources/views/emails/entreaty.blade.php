8bf490d181e336f702fe467dbb43b9b554235680
ibrahimlawal <ibrahim@lawal.me>
2016-02-24 08:57:12 +0100

    finally nailed how to make blade ignore escape newline

--- resources/views/emails/entreaty.blade.php
+++ resources/views/emails/entreaty.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 Thanks for your Business, {{ $recipient_name }}. You need to pay us &#x20a6;{{ $amount }} for {{ $invoice_title }}.
-<p>Details: {{{ nl2br(e($invoice_description)) }}}</p>
+<p>Details: {!! nl2br(e($invoice_description)) !!}</p>
 Do pay us using paystack now.
 
 @endsection
